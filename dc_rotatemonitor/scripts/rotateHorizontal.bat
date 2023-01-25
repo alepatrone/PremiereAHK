14d93570ad16db1a385e72960138aed3df159b0d
alepatrone <76615710+alepatrone@users.noreply.github.com>
2023-01-25 15:00:03 +0100

    Minor updates
    Updates shortcuts for Premiere and indexes for rotating monitor

--- dc_rotatemonitor/scripts/rotateHorizontal.bat
+++ dc_rotatemonitor/scripts/rotateHorizontal.bat
@@ -1,4 +1,4 @@
 d:
 cd "D:\Utility\dc_rotatemonitor\"
-dccmd.exe -monitor="\\.\DISPLAY2" -rotate=up -right 
-dccmd.exe  -monitor="\\.\DISPLAY2" -width=1920 -height=1080 -refresh=max -depth=max
+dccmd.exe -monitor="\\.\DISPLAY3" -rotate=up -right 
+dccmd.exe  -monitor="\\.\DISPLAY3" -width=1920 -height=1080 -refresh=max -depth=max
